c4bd86fa624e1ec4217cc45cdfbee62fde2a256e
xonirv <xonirv@gmail.com>
2018-11-01 23:07:56 +0100

    'primero'

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PreciosComponent } from './precios/precios.component';
+import { JugadoresComponent } from './jugadores/jugadores.component';
+import { DecksComponent } from './decks/decks.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'precios'   , component: PreciosComponent },
+  { path: 'decks'     , component: DecksComponent },
+  { path: 'jugadores' , component: JugadoresComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
